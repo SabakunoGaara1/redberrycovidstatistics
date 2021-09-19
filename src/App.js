93249903213af64dfd0935949dd0f78c1c03a302
torniko <tornike.nafetvaridze@gmail.com>
2021-09-19 20:28:20 +0400

    changes try to fix publish

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import { defaultFields, STEPS } from "./utils/constants";
 import { FormFieldsContext } from './utils/context/FormFieldContext';
 import { FormStepContext } from './utils/context/FormStepContext';
 import { getCurrentForm } from "./utils/helpers";
-import { BrowserRouter } from 'react-router-dom/BrowserRouter'
-
-
-
+import {
+  BrowserRouter, 
+  Route,
+  Index,
+} from "react-router-dom";
 
 function App() {
   const [step, setStep] = useState(STEPS.HELLO);
@@ -20,16 +21,15 @@ function App() {
   };
 
   return (
+  <BrowserRouter basename={window.location.pathname || ''}>
     <FormStepContext.Provider value={{step, setStep}}>
       <FormFieldsContext.Provider value={{fields, updateFields}}>
-      <BrowserRouter basename={window.location.pathname || ''}>
-       <Route exact path="/" component={Index} />
-      </BrowserRouter>
         <div className="App">
           {getCurrentForm(step)}
         </div>
       </FormFieldsContext.Provider>
     </FormStepContext.Provider>
+  </BrowserRouter>
   );
 }
 
